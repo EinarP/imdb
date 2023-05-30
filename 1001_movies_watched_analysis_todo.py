9e693b19a85b477c23c9374ea9ad6a0833ad695e
EinarP <einarpo@gmail.com>
2023-05-30 16:37:36 +0300

    Watched movies analysis added

---
+++ 1001_movies_watched_analysis_todo.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------
+# This program is for analysis of "1001 Movies to See Before you Die" list
+#
+# IMDb keeps the list in:
+#  https://www.imdb.com/list/ls060338474
+#
+# The list has been downloaded, column 'watched' added, and saved as:
+#  data/1001_movies_watched.csv
+#------------------------------------------------------------------------------
+
+# Load pandas module
+
+# Open the input file for reading (use UTF-8 encoding)
+
+# Percentage of watched movies
+
+# How many years to watch remaining movies (1 per week, 52 weeks in a years
+
+# What movie to watch next
