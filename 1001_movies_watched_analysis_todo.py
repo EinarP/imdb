0e2a3e49f3909e9cef2041adc52c52014064fb4e
EinarP <einarpo@gmail.com>
2023-05-31 11:59:32 +0300

    Elaborated 1001_movies_watched_analysis_todo.py

--- 1001_movies_watched_analysis_todo.py
+++ 1001_movies_watched_analysis_todo.py
@@ -8,12 +8,12 @@
 #  data/1001_movies_watched.csv
 #------------------------------------------------------------------------------
 
-# Load pandas module
+# Load pandas module for using it to solve the problem
 
-# Open the input file for reading (use UTF-8 encoding)
+# Open 1001_movies_watched.csv file for reading (use UTF-8 encoding)
 
-# Percentage of watched movies
+# Find percentage of watched movies (use "watched" column and total number of rows)
 
-# How many years to watch remaining movies (1 per week, 52 weeks in a years
+# How many years will take to watch remaining movies (1 movie per week, 52 weeks in a year)?
 
-# What movie to watch next
+# What movie to watch next (most highly rated unwatched movie with Genres containing "romance")?
